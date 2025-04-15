5cfb8e1f0eaefbe267539e1b9932e497c2f1a89f
piranha8899 <luca.arnold1999@gmail.com>
2025-04-15 19:16:47 +0200

    ui dashboards erweitert

--- Assets/Scripts/PanelHandler.cs
+++ Assets/Scripts/PanelHandler.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor.Build;
+#endif
 
 public class PanelHandler : MonoBehaviour
 {
