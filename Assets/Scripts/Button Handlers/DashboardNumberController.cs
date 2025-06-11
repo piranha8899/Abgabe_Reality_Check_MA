a82136fb759c767ca875bd7fc81c91052d112043
piranha8899 <luca.arnold1999@gmail.com>
2025-06-11 19:07:51 +0200

    dashboard sliders

--- Assets/Scripts/Button Handlers/DashboardNumberController.cs
+++ Assets/Scripts/Button Handlers/DashboardNumberController.cs
@@ -8,7 +8,8 @@ public class DashboardNumberController : MonoBehaviour
 {
     [Header("UI Elements")]
     public Slider householdSizeSlider;
-    public Slider savingsToggleSlider;
+    public Button redButton;
+    public Button greenButton;
     public TextMeshProUGUI kwhText;
     public TextMeshProUGUI chfText;
     public TextMeshProUGUI solarText;
@@ -17,36 +18,79 @@ public class DashboardNumberController : MonoBehaviour
     [Header("Datentabelle")]
     [SerializeField] private int[,] consumptionTable = new int[,]
     {
-        // Haushalte 1-4, [vor, nach] für kWh, CHF, Solar, Wasser
+        // Haushalte 1-4, [vor Ersparnis, nach Ersparnis] für kWh, CHF, Solar, Wasser
         { 800, 600, 240, 180, 400, 544, 100, 145 }, // 1 Person
         { 1200, 800, 360, 240, 600, 544, 180, 145 }, // 2 Personen  
         { 1800, 1200, 540, 360, 800, 700, 250, 200 }, // 3 Personen
         { 2400, 1600, 720, 480, 1000, 900, 320, 250 } // 4 Personen
     };
 
+    private bool showAfterSavings = false;
+
     void Start()
     {
         SetupSliders();
+        SetupToggleButtons();
         UpdateDisplay();
     }
 
     void SetupSliders()
     {
+        householdSizeSlider.minValue = 1;
+        householdSizeSlider.maxValue = consumptionTable.GetLength(0);
+        householdSizeSlider.wholeNumbers = true;
         householdSizeSlider.SetValueWithoutNotify(2);
-        savingsToggleSlider.SetValueWithoutNotify(1);
         
         householdSizeSlider.onValueChanged.AddListener(_ => UpdateDisplay());
-        savingsToggleSlider.onValueChanged.AddListener(_ => UpdateDisplay());
+    }
+
+    void SetupToggleButtons()
+    {
+       if (redButton != null)
+            redButton.onClick.AddListener(OnRedButtonClicked);
+        
+       if (greenButton != null)
+            greenButton.onClick.AddListener(OnGreenButtonClicked);
+    }
+
+    void OnRedButtonClicked()
+    {
+        showAfterSavings = true;
+        UpdateButtonStates();
+        UpdateDisplay();
+    }
+
+    void OnGreenButtonClicked()
+    {
+        showAfterSavings = false;
+        UpdateButtonStates();
+        UpdateDisplay();
+    }
+
+    void UpdateButtonStates()
+    {
+        if (redButton != null)
+        {
+            redButton.gameObject.SetActive(!showAfterSavings); // Rot aktiv wenn "vor Ersparnis"
+        }
+        
+        if (greenButton != null)
+        {
+            greenButton.gameObject.SetActive(showAfterSavings); // Grün aktiv wenn "nach Ersparnis"
+        }
     }
 
     void UpdateDisplay()
     {
         int household = (int)householdSizeSlider.value - 1;
-        int savings = (int)savingsToggleSlider.value;
+        int savings = showAfterSavings ? 1 : 0;
         
-        kwhText.text = consumptionTable[household, savings].ToString();
-        chfText.text = consumptionTable[household, 2 + savings].ToString();
-        solarText.text = consumptionTable[household, 4 + savings].ToString();
-        waterText.text = consumptionTable[household, 6 + savings].ToString();
+        if (household >= 0 && household < consumptionTable.GetLength(0))
+        {
+            kwhText.text = consumptionTable[household, savings].ToString();
+            chfText.text = consumptionTable[household, 2 + savings].ToString();
+            solarText.text = consumptionTable[household, 4 + savings].ToString();
+            waterText.text = consumptionTable[household, 6 + savings].ToString();
+        }
     }
 }
